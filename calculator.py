aaa6ab07562a9961eeefa1031dd062d412eb4385
Baveni <uranic.jernej@gmail.com>
2016-08-11 21:53:28 +0200

    added some gramar changes.

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 print "Welcome to my calculator, it can only perform basic math such as +, -, *, /,, enjoy :)"
-print "Please puch in the numbers you want to calculate-->>"
+print "Please do your calculations."
 while True:
 
-    x = raw_input("Please punch in the first number!")
-    operator = raw_input("Please punch in youre operator!")
-    y = raw_input("Please punch in the second number!")
+    x = raw_input("Please punch in the first number-->>")
+    operator = raw_input("Please punch in youre operator-->>")
+    y = raw_input("Please punch in the second number-->>")
 
     x = float(x)
     y = float(y)
